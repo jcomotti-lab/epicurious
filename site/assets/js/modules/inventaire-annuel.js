bd8b1d361c59630daa17021eb0ef15c23b85898c
jcomotti-lab <jcomotti@gmail.com>
2026-07-27 00:20:50 +0200

    Delete site directory

--- site/assets/js/modules/inventaire-annuel.js
+++
@@ -1,6 +0,0 @@
-import { renderReportModule } from "../components/report-view.js";
-import { REPORT_SHEETS } from "../config.js";
-
-export function render(container) {
-  renderReportModule(container, REPORT_SHEETS.inventaireAnnuel);
-}
--- site/assets/js/modules/ventes.js
+++
@@ -1,6 +0,0 @@
-import { renderCrudModule } from "../components/crud-table.js";
-import { SHEET_SCHEMAS } from "../config.js";
-
-export function render(container) {
-  renderCrudModule(container, SHEET_SCHEMAS.ventes);
-}
